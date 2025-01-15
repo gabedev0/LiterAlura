6155699c5aca4deeb4a918e45be00990a295c8b0
Gabriel Lima <gabriellimadxgg@gmail.com>
2025-01-15 17:05:53 -0300

    LiterAlura

---
+++ LiterAlura/src/main/java/com/alura/literalura/modelos/AutorDTO.java
@@ -0,0 +1,14 @@
+package com.alura.literalura.modelos;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+
+public record AutorDTO(@JsonAlias("name") String autor,
+                       @JsonAlias("birth_year") Integer anoNascimento,
+                       @JsonAlias("death_year") Integer anoFalecimento){
+    @Override
+    public String toString() {
+        return "Autor: " + autor;
+    }
+
+}
